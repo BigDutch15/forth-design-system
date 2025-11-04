6b6572230d0c7d1d566c5e168ae354e607ac1534
Wayne VanderVelde <wayne.vandervelde@northhighland.com>
2025-11-04 12:03:02 -0600

    chore: add blue to colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -68,6 +68,18 @@ export default {
           800: '#899095',
           900: '#798187',
         },
+        blue: {
+          50: '#EFF6FF',
+          100: '#DBEAFE',
+          200: '#BFDBFE',
+          300: '#93C5FD',
+          400: '#60A5FA',
+          500: '#3B82F6',
+          600: '#2563EB',
+          700: '#1D4ED8',
+          800: '#1E40AF',
+          900: '#1E3A8A',
+        },
       },
       fontFamily: {
         sans: ['Inter', 'system-ui', 'sans-serif'],
